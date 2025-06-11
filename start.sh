604083777c134e35bff769d8040216ac26a8d28a
dakaca007 <129284180+dakaca007@users.noreply.github.com>
2025-06-11 17:28:15 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 
+# 启动php-fpm服务
+service php8.1-fpm start
+# 启动Flask应用（使用gunicorn）
+#cd /var/www/html/flaskapp && gunicorn -b 0.0.0.0:8000 app:app &
+#cd /var/www/html/flaskapp && gunicorn -b 0.0.0.0:8000 -k eventlet app:app &
+cd /var/www/html/flaskapp && python3 app.py &
+# 以appuser用户启动GoTTY
+su - root -c "gotty --permit-write --port 3000 bash" &
+
+# 前台运行Nginx
+nginx -g "daemon off;"
+
+# 递归修改所有上传文件的权限
+sudo chown -R www-data:www-data /var/www/html/flaskapp/static
+sudo find /var/www/html/flaskapp/static -type f -exec chmod 644 {} \;
+chmod 755 /var/www/html/flaskapp/static/uploads
 # 启动虚拟显示
 Xvfb :0 -screen 0 ${DISPLAY_WIDTH:-1280}x${DISPLAY_HEIGHT:-720}x16 -ac +extension GLX +render -noreset &
 export DISPLAY=:0
